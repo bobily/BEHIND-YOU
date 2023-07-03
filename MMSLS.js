a1b46154a7aa3864f41fc9c49e45c084314fa0e5
bobily <64211989+bobily@users.noreply.github.com>
2023-07-03 09:08:54 +0800

    MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -7,7 +7,7 @@
 const MMSLS = [
     
     "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
+    "https://www.maimemo.com/share/page?uid=11035971&pid=a31a635985dac65985ba406308f63f1e&tid=1c121e1a471b3e8c2c569d62d79f29a2",
     "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
   ]
 =====================以上为范例！=====================
@@ -20,9 +20,9 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=11035971&pid=cb6ea4e2c16a6e8780ff7d0a54d6c0c6&tid=5052a1e93e41bce9ca1d033a22dffc9b",
-    "https://www.maimemo.com/share/page?uid=11035971&pid=cb6ea4e2c16a6e8780ff7d0a54d6c0c6&tid=5052a1e93e41bce9ca1d033a22dffc9b",
-    "https://www.maimemo.com/share/page?uid=11035971&pid=cb6ea4e2c16a6e8780ff7d0a54d6c0c6&tid=5052a1e93e41bce9ca1d033a22dffc9b",
+    "https://www.maimemo.com/share/page?uid=11035971&pid=a31a635985dac65985ba406308f63f1e&tid=1c121e1a471b3e8c2c569d62d79f29a2",
+    "https://www.maimemo.com/share/page?uid=11035971&pid=a31a635985dac65985ba406308f63f1e&tid=1c121e1a471b3e8c2c569d62d79f29a2",
+    "https://www.maimemo.com/share/page?uid=11035971&pid=a31a635985dac65985ba406308f63f1e&tid=1c121e1a471b3e8c2c569d62d79f29a2",
   ]
   /**
    * 生成随机数字
