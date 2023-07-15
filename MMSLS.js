18f77b7ff332a5b44f19f6e98870b2cdcc61bab6
bobily <64211989+bobily@users.noreply.github.com>
2023-07-16 04:23:26 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -21,7 +21,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=20240907&pid=d93c73d51c1ca60dae26589648d18bc4&tid=0a577bdc3514161adef1c8a7643e53f2",
+    "https://www.maimemo.com/share/page?uid=11035971&pid=3f0d7b869c48f46c08423e180d455f46&tid=e351a6e5396db4ecdbd1904ebb793c7f",
     "https://www.maimemo.com/share/page?uid=20240907&pid=d93c73d51c1ca60dae26589648d18bc4&tid=0a577bdc3514161adef1c8a7643e53f2",
     ]
   /**
